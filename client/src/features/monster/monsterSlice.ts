4e5bb262009bbe46cb5298e1de962db856ec7bbc
Pierce Lauring <pclauring@gmail.com>
2022-04-19 17:36:52 -0700

    Monster selector

--- client/src/features/monster/monsterSlice.ts
+++ client/src/features/monster/monsterSlice.ts
@@ -75,7 +75,7 @@ export const { updateStatus } = monsterSlice.actions;
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
-export const selectCount = (state: RootState) => state.counter.value;
+export const selectMonsters = (state: RootState) => state.monster.monsters;
 
 // We can also write thunks by hand, which may contain both sync and async logic.
 // Here's an example of conditionally dispatching actions based on current state.
